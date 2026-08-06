1630a3b6fefdd4dc705a476c96c7987a0e57c920
poshettimanishkumar <poshettymanish3@gmail.com>
2026-08-06 15:05:07 +0530

    Create day20_mobile_repair.sql

---
+++ sql/day20_mobile_repair.sql
@@ -0,0 +1,12 @@
+-- Create Database
+-- CREATE DATABASE mobile_repair_shop;
+
+-- Select Database
+USE mobile_repair_shop;
+
+-- Display all records
+SELECT * FROM mobile_repairs;
+
+-- Total Repairs
+SELECT COUNT(*) AS Total_Repairs
+FROM mobile_repairs;
